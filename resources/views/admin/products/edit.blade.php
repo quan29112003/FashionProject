fe9eea33d4c32d3b015be80c7348839f28623c17
quocviet1311 <165542203+vietdq1311@users.noreply.github.com>
2024-06-05 22:46:11 +0700

    crud product

---
+++ resources/views/admin/products/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Edit Product</h1>
+    <form action="{{ route('admin.products.update', $product) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" class="form-control" value="{{ $product->name }}" required>
+        </div>
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" class="form-control">{{ $product->description }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="price">Price:</label>
+            <input type="number" id="price" name="price" class="form-control" value="{{ $product->price }}"
+                step="0.01" required>
+        </div>
+        <div class="form-group">
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="quantity" name="quantity" class="form-control" value="{{ $product->quantity }}"
+                required>
+        </div>
+        <div class="form-group">
+            <label for="image">Image:</label>
+            <input type="file" id="image" name="image" class="form-control-file">
+            <img src="{{ asset('storage/' . $product->image) }}" width="100" class="mt-2">
+        </div>
+        <button type="submit" class="btn btn-success">Update</button>
+    </form>
+@endsection

56f718da6ee815fc147d379b2ef9c158a585ef7f
quan29112003 <quan29112003@gmail.com>
2024-06-08 13:09:36 +0700

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    
     public function index()
     {
         $users = User::all();
--- database/migrations/2024_06_05_073359_create_users_table.php
+++ database/migrations/2024_06_05_073359_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Bl23ueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nameUser');
             $table->date('birthday');

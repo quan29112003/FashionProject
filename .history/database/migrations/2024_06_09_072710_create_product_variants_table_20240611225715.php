9cf8c14ccb776cebab800eb3370e9a0c601de7b7
quan29112003 <quan29112003@gmail.com>
2024-06-11 23:10:17 +0700

    update migrate

--- .history/database/migrations/2024_06_09_072710_create_product_variants_table_20240611225715.php
+++ .history/database/migrations/2024_06_09_072710_create_product_variants_table_20240611225715.php
@@ -13,15 +13,18 @@ return new class extends Migration
     {
         Schema::create('product_variants', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('productID');
-            $table->unsignedBigInteger('colorID');
-            $table->unsignedBigInteger('sizeID');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('color_id');
+            $table->unsignedBigInteger('size_id');
             $table->integer('quantity');
             $table->decimal('price', 8, 2);
+            $table->decimal('price_sale', 8,2);
             $table->string('type');
+            $table->string('SKU');
+            $table->string('is_active');
             $table->timestamps();
 
-            $table->foreign('productID')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreign('colorID')->references('id')->on('product_colors')->onDelete('cascade');
             $table->foreign('sizeID')->references('id')->on('product_sizes')->onDelete('cascade');
         });

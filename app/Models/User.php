26e65d3ca5ca287cec0ffa23cbf3bc7d1074dae9
quan29112003 <quan29112003@gmail.com>
2024-07-12 22:48:22 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {

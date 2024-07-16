cbe7da432020da31e9aeb44dcf49488b3bc0f7ba
Nguyen Trong Duc <nguyentrongduc477@gmail.com>
2024-07-16 10:24:02 +0700

    Update header.blade.php

--- resources/views/client/partials/header.blade.php
+++ resources/views/client/partials/header.blade.php
@@ -186,44 +186,42 @@
                 {{-- login, cart, wishlist --}}
                 <div class="col-lg-3">
                     <div class="header__right">
-                        <div class="header__right__auth">
+                        <ul class="nav justify-content-end">
                             @auth
-                                <a href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="auto"
-                                        fill="currentColor" class="bi bi-person-check" viewBox="0 0 16 16">
-                                        <path
-                                            d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
-                                        <path
-                                            d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
-                                    </svg>
-                                </a>
-
-                                <a class="" href="{{ route('profile.edit') }}">Edit Profile</a>
-
-                                <a class="" href="#"
-                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
+                                {{-- edit profile --}}
+                                <li class="nav-item">
+                                    <a href="{{ route('profile.edit') }}" class="nav-link text-dark fs-5">
+                                        <i class="bi bi-person-badge"></i>
+                                    </a>
+                                </li>
+                                {{-- logout --}}
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-dark fs-6"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                </li>
                             @else
-                                <a href="{{ route('login') }}">Login /</a>
-                                <a href="{{ route('register') }}">Register</a>
-                            @endauth
-                        </div>
+                                {{-- login --}}
 
-                        <ul class="header__right__widget">
-                            <li>
-                                <a href="{{ route('wishlist.index') }}">
+                                <li class="nav-item">
+                                    <a href="{{--  javascript:void(0);--}}{{ url('login') }}" class="nav-link text-dark fs-5"
+                                        onclick="showLoginModal()">
+                                        <i class="bi bi-person-badge"></i>
+                                    </a>
+                                </li>
+                            @endauth
+
+                            {{-- Cart icons --}}
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fs-5" href="{{ route('wishlist.index') }}">
                                     <span class="icon_heart_alt"></span>
-                                    <div class="tip">2</div>
                                 </a>
                             </li>
-                            <li>
-                                <a href="{{ route('cart') }}">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark fs-5" href="{{ route('cart') }}">
                                     <span class="icon_bag_alt"></span>
                                 </a>
                             </li>

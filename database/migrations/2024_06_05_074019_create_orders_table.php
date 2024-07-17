47a97618278c9e9dd08ce26a461e861f7b5261b9
quan29112003 <quan29112003@gmail.com>
2024-07-17 22:07:10 +0700

    udpate

--- database/migrations/2024_06_05_074019_create_orders_table.php
+++ database/migrations/2024_06_05_074019_create_orders_table.php
@@ -18,13 +18,23 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->integer('status')->default(0);
             $table->decimal('total_amount', 8, 2);
+            $table->text('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->string('state')->nullable()->default('');
+            $table->string('postcode')->nullable()->default('');
+            $table->string('email')->nullable()->default('');
+            $table->text('note')->nullable()->default('');
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->unsignedBigInteger('payment_id')->nullable();
             $table->timestamps();
             $table->unsignedBigInteger('voucher_id')->nullable();
-            $table->integer('add_points');
 
             // Thiết lập khóa ngoại
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('voucher_id')->references('id')->on('vouchers')->onDelete('set null');
+            $table->foreign('status_id')->references('id')->on('statuses')->onDelete('set null');
+            $table->foreign('payment_id')->references('id')->on('payments')->onDelete('set null');
         });
     }
 

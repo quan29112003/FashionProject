bccc0a79905301eb445ad41dd2f29eb4b25ebed8
Nguyen Trong Duc <nguyentrongduc477@gmail.com>
2024-07-26 09:55:05 +0700

    delete_wishlist-menu

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -155,28 +155,6 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarWishlist" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarWishlist">
-                        <i class="ri-layout-3-line"></i>
-                        <span data-key="t-layouts">Wishlists</span>
-
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarWishlist">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.wishlists.index') }}" class="nav-link"
-                                    data-key="t-horizontal">Danh
-                                    sách</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.wishlists.create') }}" class="nav-link"
-                                    data-key="t-horizontal">Thêm mới</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarBlog" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarBlog">
                         <i class="ri-layout-3-line"></i>
